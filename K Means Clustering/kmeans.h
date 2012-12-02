c4c6d0c74d37f06acf9f44114b5191b4d587a396
Anthony Ou <whatheduck@gmail.com>
2012-12-01 21:56:08 -0700

    made those changes from today

--- K Means Clustering/kmeans.h
+++ K Means Clustering/kmeans.h
@@ -6,9 +6,32 @@
 #include <fstream>
 #include <sstream>
 #include <cmath>
+#include <map>
 
-
-
+//holds ALL units(and id of player) and the time they were made.
+struct data{
+	data(std::string unit, int frame, int player): unit(unit), frame(frame), player(player){}
+	std::string unit;
+	int frame;
+	int player;
+};
+void sort(std::map<std::pair<int, std::string>, int>);
+struct gamesummary{
+	~gamesummary(){unitsdestroyed.clear(); unitsmade.clear();}
+	std::vector<data> unitsdestroyed;
+	std::vector<data> unitsmade;
+};
+//similar to getsummary but is contained within a time interval and values are in a map.
+struct gamecountinterval{
+	gamecountinterval(){}
+	gamecountinterval(int low, int high): lowerboundry(low) , higherboundry(high){}
+	~gamecountinterval(){unitsmade.clear(); unitsdestroyed.clear();}
+	//pairs are good since they are ordered by default very well
+	std::map<std::pair<int, std::string>, int> unitsdestroyed;
+	std::map<std::pair<int, std::string>, int> unitsmade;
+	int lowerboundry, higherboundry;
+};
+gamecountinterval *	summarytointerval(int lowerboundry, int higherboundry);
 
 
 
--- K Means Clustering/kmeansparser.cpp
+++ K Means Clustering/kmeansparser.cpp
@@ -1,49 +1,125 @@
 #include "kmeans.h"
-
+#include "kmeans.cpp"
 using namespace std;
 
-string theline;
 char file[200];
-int player;
-int numberofunits;
-string unit;
+int frame, player, numberofunits;
+string unit, unit1, unit2, event, theline;
 vector<int> unitsdestroyed;
+vector<vector<int> > features;
+
+gamesummary gameSummary;
 
 int main()
 {
 	//this program requires itself to be located in the same folder as the rep.rgd files
 	//==================================
 	FILE * pipe= _popen("dir *.rep.rgd /b", "r");//use windows command dir with argument /b for simple file listing add /s for full path
-	ifstream thefile("*.rep.rgd"); //wilcard .rep.rgd to get first item
+	ifstream thefile("*.rep.rgd"); //empty string to get the first item
+	//ifstream should default construct with emtpy string... no single help place 
 	while(!feof(pipe))
 	{
-		fgets(file, 199, pipe);//file size has a character limit, but line size does not.
+		fgets(file, 199, pipe);
 		cout << file;
-		
+
 		bool flagcreated =false , flagdestroyed =false;
 		while(getline(thefile, theline))
 		{
-			if(theline == "[UNITS LOST]"){ flagdestroyed = true;}
-			if(theline == "[UNITS CREATED]"){ flagcreated = true; flagdestroyed = false;}
+			if(theline == "[UNITS LOST]"){ break;}
+			istringstream stream(theline);
+			//unit race >> namestring >> sometimes they are 3 string names
+			stream >> frame >>player>> event >> unit >> unit;
+			if(frame !=0 && player != -1){
+				if(event == "Created")
+				{
+					stream >> unit1;
+					if(!stream.fail())
+					{
+						data Event(unit+unit1, frame, player);
+						gameSummary.unitsmade.push_back(Event);
 
-			if(flagdestroyed == true)
-			{
-				istringstream stream(theline);
-				stream >> player >> unit >> unit >> numberofunits;
-				//cout <<  player<< " " << unit<< " " <<" " <<numberofunits;
-				//something to do with vector unitsdestroyed
-			}
-			if(flagcreated == true)
-			{
-				istringstream stream(theline);
-				stream >> player >> unit >> unit >> numberofunits;
-				//cout <<  player<< " " << unit<< " " <<" " <<numberofunits;
-				//something to do with vector unitsmade.
+						stream >> unit2;
+						if(!stream.fail())
+						{
+							data Event(unit+unit1+unit2, frame, player);
+							gameSummary.unitsmade.push_back(Event);
+						}
+					}
+				}
+				if(event =="Destroyed")
+				{
+					stream >> unit1;
+					if(!stream.fail())
+					{
+						data Event(unit+unit1, frame, player);
+						gameSummary.unitsdestroyed.push_back(Event);
+
+						if(!stream.fail()){
+							stream >> unit2;
+							data Event(unit+unit1+unit2, frame, player);
+							gameSummary.unitsdestroyed.push_back(Event);
+
+						}
+					}
+
+				}
+
 			}
 		}
+
+		gamecountinterval * interval = summarytointerval(1, 8000);
+		sort(interval->unitsmade);
+		gameSummary.unitsdestroyed.clear();
+		gameSummary.unitsmade.clear();
 		thefile.close();
 		thefile.open(file);
 	} 
 	_pclose(pipe);
 	//==================================
 }
+
+gamecountinterval * summarytointerval(int lowerboundry, int higherboundry)
+{
+	gamecountinterval *temp = new gamecountinterval(lowerboundry, higherboundry);
+	std::map<std::pair<int, std::string>, int>::iterator it;
+	int size = gameSummary.unitsdestroyed.size();
+	for(int i =0; i< size; ++i){
+		data var = gameSummary.unitsdestroyed[i]; 
+		if(var.frame < higherboundry && var.frame >= lowerboundry){
+			std::pair<int, std::string> IDandunit = std::make_pair(var.player, var.unit);
+			it = temp->unitsdestroyed.find(IDandunit);
+			if(it == temp->unitsdestroyed.end())
+				temp->unitsdestroyed[IDandunit] = 1;
+			else
+				++it->second;
+		}
+	}
+	size = gameSummary.unitsmade.size();
+	for(int i = 0;i< size;++i){
+		data var = gameSummary.unitsmade[i]; 
+		if(var.frame < higherboundry && var.frame >= lowerboundry){
+			std::pair<int, std::string> IDandunit = std::make_pair(var.player, var.unit);
+			it = temp->unitsmade.find(IDandunit);
+			if(it == temp->unitsmade.end())
+				temp->unitsmade[IDandunit] = 1;
+			else
+				++it->second;
+		}
+	}
+	return temp;
+}
+enum Protossfeature{
+	Arbiter,Archon,Arbitertribunal,Assimilator,
+	Carrier,Corsair,CitadelofAdun,Cyberneticscore,
+	Darkarchon,Darktemplar,Dragoon,
+	Hightemplar,Interceptor,Observer,Probe,Reaver,
+	Scout,Shuttle,Zealot,Gateway,Nexus,
+	Observatory,Fleetbeacon,Forge,Photoncannon,
+	Pylon,Roboticsfacility,Roboticssupportbay,
+	Shieldbattery,Stargate,Templararchives,
+};
+void sort(std::map<std::pair<int, std::string>, int>)
+{
+
+}
+
